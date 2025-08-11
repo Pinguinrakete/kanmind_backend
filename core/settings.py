248d7b9aefccc8c5a6acca41d5a753d45205dc38
Pinguinrakete <ekirschke@web.de>
2025-08-11 12:50:51 +0200

    update readme.md

--- core/settings.py
+++ core/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
-    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'auth_app',
     'kanban_app',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -57,16 +57,6 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True 
-
-CSRF_TRUSTED_ORIGINS = [
-  'http://127.0.0.1:5500',
-  'http://localhost:5500',
-]
-
-CORS_ALLOWED_ORIGINS = [
-  'http://127.0.0.1:5500',
-  'http://localhost:5500',
-]
 
 ROOT_URLCONF = 'core.urls'
 
